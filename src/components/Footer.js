cc2a2c20e33917fdee2674a14f745df495fd2c66
supriyo nag <supriyonag522@gmail.com>
2025-01-03 17:48:52 +0530

    file in map part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,6 +51,7 @@ const Footer = () => {
         </div>
         <div className="footerRight">
           <iframe
+            className="mapIframe"
             title="EEC Learning App Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.5107236991316!2d88.350999!3d22.559994000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027708e4bbaea9%3A0xcac9c767f56f9985!2s19B%2C%20Jawaharlal%20Nehru%20Rd%2C%20New%20Market%20Area%2C%20Dharmatala%2C%20Taltala%2C%20Kolkata%2C%20West%20Bengal%20700087!5e0!3m2!1sen!2sin!4v1735879197476!5m2!1sen!2sin"
             width="100%"
